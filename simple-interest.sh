0cc0802c1b0a8d45547cd10c448d284aacde2885
Safa Campwala <safacampwala@gmail.com>
2026-09-04 09:08:08 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -9,7 +9,6 @@ read -p "Enter Principal Amount: " principal
 read -p "Enter Rate of Interest (in %): " rate
 read -p "Enter Time Period (in years): " time
 
-# Calculate Simple Interest = (P x R x T) / 100
 simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
 
 echo "----------------------------"
